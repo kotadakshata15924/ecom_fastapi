995de7deddc977ef886ce0eb6c434284bef6fe83
kotadakshata15924 <149565983+kotadakshata15924@users.noreply.github.com>
2026-07-04 17:17:53 +0530

    Delete product_service directory

--- product_service/database/connection.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-# each microservice owns its own database
-engine = create_engine("sqlite:///product_service.db")
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
